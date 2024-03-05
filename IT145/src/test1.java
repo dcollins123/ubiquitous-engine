3a28735b15d01e99bcb5e494bef4c8deb010b903
Daniel Collins <126200394+dcollins123@users.noreply.github.com>
2024-03-05 09:31:24 -0500

    Add files via upload

---
+++ IT145/src/test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		do {
+			System.out.println(counter);
+			counter++;
+			
+		}while (counter <=10);
+			
+	}
+}
